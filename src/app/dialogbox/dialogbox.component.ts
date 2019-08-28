ebc3053edf5648de69809920eba8ffb7a4873373
aashishjuyal <47942416+aashishjuyal@users.noreply.github.com>
2019-08-28 08:04:37 +0530

    download changes

--- src/app/dialogbox/dialogbox.component.ts
+++ src/app/dialogbox/dialogbox.component.ts
@@ -39,15 +39,31 @@ export class DialogBox {
   downloadFile(DocumentNameWithGuid,Id){
     this.service.downloadFile(DocumentNameWithGuid,Id)
     .subscribe(response => {
-      console.log(response);
+      downloadFile1(response);
     });
-    //this.fileService.downloadFile().subscribe(response => {
-			//let blob:any = new Blob([response.blob()], { type: 'text/json; charset=utf-8' });
-			//const url= window.URL.createObjectURL(blob);
-			//window.open(url);
-		//	window.location.href = response.url;
-			//fileSaver.saveAs(blob, 'employees.json');
-		//});
   }
+  downloadFile1(t) {
+    var e = t.headers.get("content-disposition").split("=")[1]
+      , n = (e = e.substring(1, e.length)).split(".")[1].toLowerCase()
+      , r = new Blob([t.body],{
+        type: this.createFileType(n)
+    });
+    if (window.navigator && window.navigator.msSaveOrOpenBlob)
+        window.navigator.msSaveOrOpenBlob(r);
+    else {
+        var i = window.URL.createObjectURL(r)
+          , o = document.createElement("a");
+        document.body.appendChild(o),
+        o.href = i,
+        o.download = e,
+        o.click(),
+        window.URL.revokeObjectURL(i)
+    }
+}
+createFileType(t) {
+    var e = "";
+    return "pdf" == t || "csv" == t ? e = "application/" + t : "jpeg" == t || "jpg" == t || "png" == t ? e = "image/" + t : "txt" == t ? e = "text/plain" : "ppt" == t || "pot" == t || "pps" == t || "ppa" == t ? e = "application/vnd.ms-powerpoint" : "pptx" == t ? e = "application/vnd.openxmlformats-officedocument.presentationml.presentation" : "doc" == t || "dot" == t ? e = "application/msword" : "docx" == t ? e = "application/vnd.openxmlformats-officedocument.wordprocessingml.document" : "xls" == t || "xlt" == t || "xla" == t ? e = "application/vnd.ms-excel" : "xlsx" == t && (e = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"),
+    e
+}
 
 }
