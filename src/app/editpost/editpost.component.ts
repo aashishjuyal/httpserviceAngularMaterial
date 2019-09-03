5b1db7557ac86c2fa9b67c8614144a823b668e8e
kadwalbhavana <aashishjuyal.aj@gmail.com>
2019-09-03 11:38:40 +0530

    date format change

--- src/app/editpost/editpost.component.ts
+++ src/app/editpost/editpost.component.ts
@@ -108,7 +108,8 @@ export class EditpostComponent implements OnInit {
           element.name = element.DocumentName;
           this.files.push(element)
         }
-        this.minDate = moment(this.post.Content.Result.RaisedOn).toDate();
+        //this.minDate = moment(this.post.Content.Result.RaisedOn).toDate();
+        this.editPostForm.get("RaisedOn").clearValidators();
         this.editPostForm.setValue({
           'CrId': this.post.Content.Result.ProjectId+'-'+this.post.Content.Result.CrId,
           'CrEditId':this.post.Content.Result.CrId,
